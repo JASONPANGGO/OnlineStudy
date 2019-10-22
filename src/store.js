7d8bcfe5cdde5d4b63150efad41ce867e631b6d3
JasonPang <jason_pang@126.com>
2019-10-22 08:44:21 +0800

    完成首页组件

--- src/store.js
+++ src/store.js
@@ -3,7 +3,8 @@ import Vue from 'vue'
 export let store = Vue.observable({
     appName: 'OnlineStudy',
     appLogo: 'el-icon-s-promotion',
-    appSlogan: "Build intelligent apps of the future"
+    appSlogan: "Build intelligent apps of the future",
+    themeColor:'rgb(0, 219, 110)'
 })
 
 export let mutations = {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,25 +8,42 @@
       </div>
       <div class="slogan">{{appSlogan}}</div>
     </div>
+    <div class="videos">
+
+
+      <VideoItem v-for="(video, index) in videos" :key="index" :title="video.title" :author="video.author" :imgSrc="video.imgSrc" :star="video.star"/>
+
+
+      <div class="refresh">
+          <i class="el-icon-refresh"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { store, mutations } from "../store";
-
+import VideoItem from "../components/VideoItem";
 // @ is an alias to /src
 export default {
   name: "home",
-  components: {},
+  components: {
+    VideoItem
+  },
   data() {
     return {
-
+        videos:[
+            {title:'HTML高级教程',author:'Mike Flog',imgSrc:"http://placehold.it/230x150",star:"9.6",id:"001"},
+            {title:'CSS入门',author:'Will Tune',imgSrc:"http://placehold.it/230x150",star:"5.6",id:"002"},
+            {title:'JavaScript进阶',author:'Trenise Ruive',imgSrc:"http://placehold.it/230x150",star:"8.4",id:"003"},
+            {title:'TensorFlow从入门到精通',author:'Kiutruse Vlowers',imgSrc:"http://placehold.it/230x150",star:"7.7",id:"004"}
+        ]
     };
   },
   computed: {
     appName: () => store.appName,
     appLogo: () => store.appLogo,
-    appSlogan: ()=> store.appSlogan
+    appSlogan: () => store.appSlogan
   }
 };
 </script>
@@ -41,8 +58,9 @@ export default {
 video {
   position: fixed;
   margin-left: 60px;
-  height: 100vh;
-  width: 100vw;
+  width: 1920px;
+   user-select: none;   
+  /* height: 100vh; */
 }
 
 .logo {
@@ -67,5 +85,36 @@ video {
   color: white;
   width: 400px;
   font-weight: bold;
+}
+
+.videos {
+  position: absolute;
+  width: calc(100vw - 60px);
+  margin-left: 60px;
+  height: 300px;
+  bottom: 0;
+  background-color: aqua;
+  background: linear-gradient(transparent, #111111);
+
+  display: flex;
+  align-items: center;
+}
+
+.refresh{
+    margin-left: 70px;
+    font-size: 3rem;
+    color: white;
+    transition: 0.3s all;
+    cursor: pointer;
+}
+.refresh:hover{
+    animation: spin infinite 1s reverse;
+}
+@keyframes spin {
+    
+		0%{-webkit-transform:rotate(0deg);}
+
+		100%{-webkit-transform:rotate(360deg);}
+ 
 }
 </style>
